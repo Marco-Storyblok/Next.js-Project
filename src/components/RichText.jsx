2dbcc54ebff3244521fa233cc5dc2d750e122e38
Marco-Storyblok <marco.sanabria@storyblok.com>
2026-08-27 13:22:13 -0600

    Update RichText renderer

--- src/components/RichText.jsx
+++ src/components/RichText.jsx
@@ -6,16 +6,7 @@ import {
 export default function RichText({ blok }) {
   return (
     <div {...storyblokEditable(blok)}>
-      <StoryblokServerRichText
-        document={blok.text}
-        optimizeImage={{
-          width: 768,
-          filters: {
-            format: "avif",
-            quality: 80,
-          },
-        }}
-      />
+      <StoryblokServerRichText document={blok.text} />
     </div>
   );
 }
