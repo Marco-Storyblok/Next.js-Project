f7b54d1f768a6d1a1c9ff30d770b85f836baed3a
Marco-Storyblok <marco.sanabria@storyblok.com>
2026-08-27 13:05:32 -0600

    Update RichText image rendering

--- src/components/RichText.jsx
+++ src/components/RichText.jsx
@@ -1,41 +1,19 @@
 import {
   storyblokEditable,
-  renderRichText,
+  StoryblokServerRichText,
 } from "@storyblok/react/rsc";
 
 export default function RichText({ blok }) {
-  const html = renderRichText(blok.text, {
-    resolver: (component, blok) => {
-      return undefined;
-    },
-    nodeResolvers: {
-      image: (node) => {
-        const src = node?.attrs?.src;
-
-        if (!src) {
-          return "";
-        }
-
-        const transformedSrc =
-          `${src}/m/768x0/filters:format(avif):no_upscale():quality(80)`;
-
-        return `
-          <img
-            src="${transformedSrc}"
-            alt="${node.attrs.alt || ""}"
-            title="${node.attrs.title || ""}"
-            style="max-width:100%;height:auto;"
-          />
-        `;
-      },
-    },
-  });
-
   return (
     <div {...storyblokEditable(blok)}>
-      <div
-        dangerouslySetInnerHTML={{
-          __html: html,
+      <StoryblokServerRichText
+        document={blok.text}
+        optimizeImage={{
+          width: 768,
+          filters: {
+            format: "avif",
+            quality: 80,
+          },
         }}
       />
     </div>
