64241763d711c0e8541c9827ada3d5878457e1b5
Marco-Storyblok <marco.sanabria@storyblok.com>
2026-08-27 13:32:56 -0600

    Update RichText renderer

--- src/components/RichText.jsx
+++ src/components/RichText.jsx
@@ -1,12 +1,12 @@
-import {
-  storyblokEditable,
-  StoryblokServerRichText,
-} from "@storyblok/react/rsc";
+import { storyblokEditable } from "@storyblok/react/rsc";
+import { renderRichText } from "@storyblok/richtext";
 
 export default function RichText({ blok }) {
+  const html = renderRichText(blok.text);
+
   return (
     <div {...storyblokEditable(blok)}>
-      <StoryblokServerRichText document={blok.text} />
+      <div dangerouslySetInnerHTML={{ __html: html }} />
     </div>
   );
 }
